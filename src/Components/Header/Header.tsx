6c8896951b0342749375b56651a1e92e0baf9918
silvershade1 <silvershade1000@gmail.com>
2024-10-12 19:28:33 +0330

    feat : add navbar

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,19 +1,63 @@
-// src/components/Header.js
 import "./Header.css";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 function Header() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
+
+  const handleLanguageChange = (event) => {
+    const newLanguage = event.target.value;
+    setSelectedLanguage(newLanguage);
+    i18n.changeLanguage(newLanguage);
+  };
 
   return (
     <header className="header">
-      <div className="infoContainer">
-        <h1>{t("header.greeting")}</h1>
-        <p>{t("header.aboutMe")}</p>
-        <p>{t("header.description")}</p>
-      </div>
-      <div className="imageContainer">
-        <img src="/Programmer-amico.svg" alt="Programmer" className="image" />
+      <nav className="navbar">
+        <div className="navbar__logo">My Portfolio</div>
+        <ul className="navbar__links">
+          <li className="navbar__item">
+            <a href="#home" className="navbar__link">
+              {t("navbar.home")}
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="#projects" className="navbar__link">
+              {t("navbar.projects")}
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="#about" className="navbar__link">
+              {t("navbar.about")}
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="#contact" className="navbar__link">
+              {t("navbar.contact")}
+            </a>
+          </li>
+        </ul>
+        <div className="language-selector">
+          <select
+            value={selectedLanguage}
+            onChange={handleLanguageChange}
+            className="language-selector__dropdown"
+          >
+            <option value="en">English</option>
+            <option value="fa">فارسی</option>
+          </select>
+        </div>
+      </nav>
+      <div className="contentWrapper">
+        <div className="infoContainer">
+          <h1>{t("header.greeting")}</h1>
+          <p>{t("header.aboutMe")}</p>
+          <p>{t("header.description")}</p>
+        </div>
+        <div className="imageContainer">
+          <img src="/Programmer-amico.svg" alt="Programmer" className="image" />
+        </div>
       </div>
     </header>
   );

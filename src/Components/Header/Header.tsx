0f5bd4d03a2babe3be6043784f170e5255d00112
silvershade1 <silvershade1000@gmail.com>
2024-11-16 18:44:42 +0330

    feat : add LanguageSelector

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,35 +1,17 @@
 import "./Header.css";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+
 import Lottie from "lottie-react";
 import animationData from "../../asset/Animation - 1731768441890.json";
+import LanguageSelector from "../LanguageSelector/LanguageSelector";
 
 function Header() {
-  const { t, i18n } = useTranslation();
-  const [selectedLanguage, setSelectedLanguage] = useState("en");
-
-  const handleLanguageChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const newLanguage = event.target.value;
-    setSelectedLanguage(newLanguage);
-    i18n.changeLanguage(newLanguage);
-  };
-
+  const { t } = useTranslation();
   return (
     <header className="header">
       <div className="navbar">
         <div className="navbar__logo">My Portfolio</div>
-        <div className="language-selector">
-          <select
-            value={selectedLanguage}
-            onChange={handleLanguageChange}
-            className="language-selector__dropdown"
-          >
-            <option value="en">English</option>
-            <option value="fa">فارسی</option>
-          </select>
-        </div>
+        <LanguageSelector />
       </div>
       <div className="contentWrapper">
         <div className="infoContainer">
---
+++ src/Components/LanguageSelector/LanguageSelector.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import "./LanguageSelector.css";
+
+const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
+
+  const handleLanguageChange = (language: string) => {
+    i18n.changeLanguage(language);
+    setSelectedLanguage(language);
+    setIsDropdownOpen(false);
+  };
+
+  const languageOptions =
+    selectedLanguage === "en"
+      ? [{ code: "fa", label: "فارسی", flag: "/iranFlag.png" }]
+      : [{ code: "en", label: "EN", flag: "/usaFlag.png" }];
+
+  return (
+    <div className="language-selector">
+      <button
+        type="button"
+        aria-haspopup="menu"
+        aria-expanded={isDropdownOpen ? "true" : "false"}
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        className="language-selector__button"
+      >
+        <div className="flex items-center gap-4">
+          <img
+            src={selectedLanguage === "en" ? "/usaFlag.png" : "/iranFlag.png"}
+            alt={selectedLanguage}
+            className="flag-icon"
+          />
+          <span>{selectedLanguage === "en" ? "EN" : "فارسی"}</span>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="25"
+          fill="none"
+          viewBox="0 0 24 25"
+          className="dropdown-icon"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m6 9.5 6 6 6-6"
+          ></path>
+        </svg>
+      </button>
+
+      {isDropdownOpen && (
+        <div className="dropdown-menu">
+          {languageOptions.map((language) => (
+            <div
+              key={language.code}
+              onClick={() => handleLanguageChange(language.code)}
+              className="dropdown-item"
+            >
+              <img
+                src={language.flag}
+                alt={language.label}
+                className="flag-icon"
+              />
+              {language.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LanguageSelector;

9fb2e680185d93a279c417aab78866e287b532aa
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-12-25 13:22:22 +0330

    feat : add background pattern

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${exo2.variable} antialiased scroll-smooth w-full max-w-dvw overflow-x-hidden`}
+        className={`${exo2.variable} antialiased scroll-smooth  bg-[url('/topography.svg')] bg-repeat w-full max-w-dvw overflow-x-hidden`}
       >
         <SmoothCursor />
 
